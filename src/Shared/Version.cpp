54a7ca5e4803b9cd989812caa3b6de9768bf006c
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-07 10:54:25 +0200

    prepare to test package<->source switching in app

---
+++ src/Shared/Version.cpp
@@ -0,0 +1,6 @@
+#include "Version.h"
+
+std::string GetVersionDetailed()
+{
+    return "Version 0.1.0 - TODO: Build Info";
+}
--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,13 @@ add_rules("mode.debug", "mode.release")
 
 target("tx-lib-shared")
     set_kind("static")
+
     add_files("src/foo.cpp")
     add_headerfiles("src/foo.h")
+
+    add_files("src/Shared/Version.cpp")
+    add_headerfiles("src/Shared/Version.h", {prefixdir = "Shared"})
+
     add_includedirs("src", {public = true})
 
 -- target("tx-lib-shared-demo")
