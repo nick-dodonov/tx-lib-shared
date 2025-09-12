fc76d957f978b1ef359f029973619be5f330b62c
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-12 08:36:03 +0200

    try add sub dependecy

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,12 @@
 add_rules("mode.debug", "mode.release")
 set_languages("c++17")
 
+add_repositories("tx-kit-repo " .. (os.getenv("TX_REPO") or "git@github.com:nick-dodonov/tx-kit-repo.git main"))
+add_requires("tx-kit")
+includes(".xmake/tx-kit/includes")
+
+add_requires("tx-pkg-aux")
+
 target("tx-pkg-misc", function()
     set_kind("static")
 
@@ -8,4 +14,6 @@ target("tx-pkg-misc", function()
     add_headerfiles("src/Shared/Version.h", {prefixdir = "Shared"})
 
     add_includedirs("src", {public = true})
+
+    add_packages("tx-pkg-aux")
 end)
