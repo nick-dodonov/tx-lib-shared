455171cfab2e71807d2634d0435a5a788f80bfe8
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-10-04 18:16:10 +0200

    demo uses aux module pkg lib (stub w/o registry) - simply and it works

--- demo/hello-pkg/main.cpp
+++ demo/hello-pkg/main.cpp
@@ -1,20 +1,10 @@
 #define _POSIX_C_SOURCE 200809L // for getcwd todo: move to bazel copts
 #include <stdio.h>
 #include <unistd.h>
+#include "Boot/Boot.h"
 
 int main(int argc, char** argv)
 {
-    printf("TODO: hello-pkg (dep to personal bazel module)\n");
-    // to verify args and cwd from launch.json
-    for (int i = 0; i < argc; i++) {
-        printf("  [%d]: %s\n", i, argv[i]);
-    }
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("  dir: %s\n", cwd);
-    } else {
-        perror("  dir: getcwd() error");
-    }
-
+    Boot::LogInfo(argc, argv);
     return argc - 1;
 }
