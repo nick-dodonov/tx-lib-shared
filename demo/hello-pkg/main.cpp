0e17cdc7636de5755fb43c95fe2afa40d08a4bac
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-10-04 15:37:29 +0200

    add vscode build task & debug launch stubs for demo

--- demo/hello-pkg/main.cpp
+++ demo/hello-pkg/main.cpp
@@ -1,7 +1,19 @@
+#define _POSIX_C_SOURCE 200809L // for getcwd todo: move to bazel copts
 #include <stdio.h>
+#include <unistd.h>
 
-int main()
+int main(int argc, char** argv)
 {
     printf("TODO: hello-pkg (dep to personal bazel module)\n");
+    // to verify args and cwd from launch.json
+    for (int i = 0; i < argc; i++) {
+        printf("  [%d]: %s\n", i, argv[i]);
+    }
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("  dir: %s\n", cwd);
+    } else {
+        perror("  dir: getcwd() error");
+    }
     return 0;
 }
