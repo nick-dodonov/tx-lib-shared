82535318b7556cdc961390395122d6c9262ab75b
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-10-20 23:23:36 +0200

    Add AsioTest to coro_test.cpp to demonstrate async timer usage (w/o threadings usage yet)

--- test/coro_test.cpp
+++ test/coro_test.cpp
@@ -1,9 +1,26 @@
 #include "CoroTest.h"
+#include <asio.hpp>
+#include <format>
 
 TEST_F(CoroTest, Sub) {
     Log::Debug("Sub: start");
-    co_await delay_for(std::chrono::milliseconds(500), synCtx);
+    co_await delay_for(std::chrono::milliseconds(200), synCtx);
     Log::Debug("Sub: after delay");
-    co_await delay_for(std::chrono::milliseconds(500), synCtx);
+    co_await delay_for(std::chrono::milliseconds(200), synCtx);
     Log::Debug("Sub: end");
+}
+
+static void print(const std::error_code& ec) {
+    Log::Debug(std::format("Try: print: {}", ec.message()));
+}
+
+TEST(AsioTest, Try) {
+    Log::Debug("Try: start");
+
+    asio::io_context io;
+    asio::steady_timer t(io, asio::chrono::milliseconds(200));
+    t.async_wait(&print);
+    io.run();
+
+    Log::Debug("Try: end");
 }
