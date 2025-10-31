9765864a12f37cf3d49a48be41a42a11a182dcd6
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-11-01 00:46:13 +0100

    next asio-try w/ timer restart & param binds
    tutor 3,4

--- demo/asio-try/main.cpp
+++ demo/asio-try/main.cpp
@@ -1,24 +1,54 @@
 #include "Boot/Boot.h"
 #include "Log/Log.h"
 #include <asio.hpp>
+#include <functional>
 
 using namespace std::chrono_literals;
 
-void print(const std::error_code& ec)
+class printer
 {
-    Log::Info(std::format("Asio callback: {}.{}", ec.category().name(), ec.message()));
+public:
+    printer(asio::io_context& io)
+        : timer_(io, asio::chrono::seconds(1))
+        , count_(0)
+    {
+        timer_.async_wait(std::bind(&printer::print, this, asio::placeholders::error));
+    }
+    ~printer() {
+        Log::Info(std::format("Final count is {}", count_));
+    }
+
+    void print(const std::error_code& ec)
+    {
+        if (count_ < 5) {
+            Log::Info(std::format("print: [{}] {}", count_, ec.message()));
+            ++count_;
+
+            timer_.expires_at(timer_.expiry() + asio::chrono::seconds(1));
+            timer_.async_wait(std::bind(&printer::print, this, asio::placeholders::error));
+        }
+    }
+
+private:
+    asio::steady_timer timer_;
+    int count_;    
+};
+
+void print(const std::error_code& ec, asio::steady_timer* t, int* count)
+{
+    Log::Info(std::format("Asio callback: [{}] {}.{}", *count, ec.category().name(), ec.message()));
+    if (*count < 5) {
+        ++(*count);
+        t->expires_at(t->expiry() + asio::chrono::seconds(1));
+        t->async_wait(std::bind(print, asio::placeholders::error, t, count));
+    }
 }
 
 int main(int argc, char** argv)
 {
     Boot::LogInfo(argc, argv);
     asio::io_context io;
-    asio::steady_timer t(io, 5s);
-
-    //t.wait();
-    t.async_wait(&print);
+    printer p(io);
     io.run();
-
-    Log::Info("waited");
     return 0;
 }
