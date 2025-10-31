96200921bbb777773c70dc68c15090267c761a36
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-11-01 00:28:38 +0100

    add settings to run/debug asio demo and all tests via vscode C++TestMate extension (via gutter icon)
    asio tutor 2

--- demo/asio-try/main.cpp
+++ demo/asio-try/main.cpp
@@ -2,12 +2,23 @@
 #include "Log/Log.h"
 #include <asio.hpp>
 
+using namespace std::chrono_literals;
+
+void print(const std::error_code& ec)
+{
+    Log::Info(std::format("Asio callback: {}.{}", ec.category().name(), ec.message()));
+}
+
 int main(int argc, char** argv)
 {
     Boot::LogInfo(argc, argv);
     asio::io_context io;
-    asio::steady_timer t(io, asio::chrono::seconds(5));
-    t.wait();
+    asio::steady_timer t(io, 5s);
+
+    //t.wait();
+    t.async_wait(&print);
+    io.run();
+
     Log::Info("waited");
     return 0;
 }
