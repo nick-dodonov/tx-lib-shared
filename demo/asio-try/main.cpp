40a3eabeaa7ad8592964fc5e63261992396f6e45
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-11-01 17:01:23 +0300

    next asio-try w/ simple sync tcp server
    tutor Daytime.2 extended by Daytime.1

--- demo/asio-try/main.cpp
+++ demo/asio-try/main.cpp
@@ -1,9 +1,38 @@
 #include "Boot/Boot.h"
-#include <array>
-#include <iostream>
+#include "Log/Log.h"
 #include <asio.hpp>
+#include <array>
+#include <ctime>
 
 using asio::ip::tcp;
+
+static std::string make_daytime_string() {
+    std::time_t now = time(0);
+    return std::ctime(&now);
+}
+
+int main_server()
+{
+    asio::io_context io_context;
+
+    Log::Info("initializing");
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
+
+    for (;;) {
+        Log::Info("listening");
+
+        tcp::socket socket(io_context);
+        acceptor.accept(socket);
+
+        Log::Info("accepted");
+        std::string message = make_daytime_string();
+
+        Log::Info(std::format("writing: {}", message));
+        std::error_code ignored_error;
+        asio::write(socket, asio::buffer(message), ignored_error);
+    }
+    return 0;
+}
 
 int main(int argc, char** argv)
 {
@@ -11,10 +40,16 @@ int main(int argc, char** argv)
 
     // try {
         if (argc != 2) {
-            std::cerr << "Usage: client <host>" << std::endl;
-            std::cerr << "Service can be emulated: `while true; do echo -ne \"$(date -u)\\0\" | nc -l 13; echo accessed; done`" << std::endl;
+            Log::Error(std::format("Usage: {} {{-s|<host>}}", argv[0]));
+            Log::Error("Service emulation: `while true; do echo -ne \"$(date -u)\\0\" | nc -l 13; echo accessed; done`");
             return 1;
         }
+
+        auto arg = argv[1];
+        if (strcmp(arg, "-s") == 0) {
+            return main_server();
+        }
+
         asio::io_context io_context;
         tcp::resolver resolver(io_context);
         tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
@@ -26,16 +61,15 @@ int main(int argc, char** argv)
 
             size_t len = socket.read_some(asio::buffer(buf), error);
             if (error == asio::error::eof) {
-                std::cout << "closed normally\n";
+                Log::Info("closed normally");
                 break; // Connection closed cleanly by peer.
             } else if (error) {
                 //throw std::system_error(error); // Some other error.
-                std::cerr << "closed w/ error: " << error.message() << std::endl;
+                Log::Error(std::format("closed w/ error: {}", error.message()));
                 break;
             }
 
-            std::cout.write(buf.data(), len);
-            std::cout << "\n";
+            Log::Info(std::format("Result: {}", std::string_view(buf.data(), len)));
         }
     // } catch(std::exception ex) {
     //     std::cerr << "EXCEPTION: " << ex.what();
