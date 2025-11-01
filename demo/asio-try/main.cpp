16869e146b0905ad729b62d3911f78c3ff1492a3
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-11-01 01:16:17 +0100

    next asio-try w/ threads and strand
    tutor 5

--- demo/asio-try/main.cpp
+++ demo/asio-try/main.cpp
@@ -9,46 +9,49 @@ class printer
 {
 public:
     printer(asio::io_context& io)
-        : timer_(io, asio::chrono::seconds(1))
+        : strand_(asio::make_strand(io))
+        , timer1_(io, asio::chrono::seconds(1))
+        , timer2_(io, asio::chrono::seconds(1))
         , count_(0)
     {
-        timer_.async_wait(std::bind(&printer::print, this, asio::placeholders::error));
+        timer1_.async_wait(asio::bind_executor(strand_,
+            std::bind(&printer::print, this, asio::placeholders::error, &timer1_)));
+        timer2_.async_wait(asio::bind_executor(strand_,
+            std::bind(&printer::print, this, asio::placeholders::error, &timer2_)));
     }
     ~printer() {
         Log::Info(std::format("Final count is {}", count_));
     }
 
-    void print(const std::error_code& ec)
+    void print(const std::error_code& ec, asio::steady_timer* timer)
     {
         if (count_ < 5) {
             Log::Info(std::format("print: [{}] {}", count_, ec.message()));
             ++count_;
 
-            timer_.expires_at(timer_.expiry() + asio::chrono::seconds(1));
-            timer_.async_wait(std::bind(&printer::print, this, asio::placeholders::error));
+            timer->expires_at(timer->expiry() + asio::chrono::seconds(1));
+            timer->async_wait(asio::bind_executor(strand_,
+                std::bind(&printer::print, this, asio::placeholders::error, timer)));
         }
     }
 
 private:
-    asio::steady_timer timer_;
-    int count_;    
+  asio::strand<asio::io_context::executor_type> strand_;
+  asio::steady_timer timer1_;
+  asio::steady_timer timer2_;
+  int count_;
 };
 
-void print(const std::error_code& ec, asio::steady_timer* t, int* count)
-{
-    Log::Info(std::format("Asio callback: [{}] {}.{}", *count, ec.category().name(), ec.message()));
-    if (*count < 5) {
-        ++(*count);
-        t->expires_at(t->expiry() + asio::chrono::seconds(1));
-        t->async_wait(std::bind(print, asio::placeholders::error, t, count));
-    }
-}
 
 int main(int argc, char** argv)
 {
     Boot::LogInfo(argc, argv);
+
     asio::io_context io;
     printer p(io);
+    std::thread t([&]{ io.run(); });
     io.run();
+    t.join();
+
     return 0;
 }
