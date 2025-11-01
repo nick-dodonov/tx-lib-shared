df7f7cb8bceacb587c366d0126c86f6a4170ca94
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-11-01 13:32:42 +0100

    Refactor asio-try demo as TCP client and update LLDB launch settings (simplify execution in vscode)
    tutor Daytime.1 extended

--- demo/asio-try/main.cpp
+++ demo/asio-try/main.cpp
@@ -1,57 +1,44 @@
 #include "Boot/Boot.h"
-#include "Log/Log.h"
+#include <array>
+#include <iostream>
 #include <asio.hpp>
-#include <functional>
 
-using namespace std::chrono_literals;
-
-class printer
-{
-public:
-    printer(asio::io_context& io)
-        : strand_(asio::make_strand(io))
-        , timer1_(io, asio::chrono::seconds(1))
-        , timer2_(io, asio::chrono::seconds(1))
-        , count_(0)
-    {
-        timer1_.async_wait(asio::bind_executor(strand_,
-            std::bind(&printer::print, this, asio::placeholders::error, &timer1_)));
-        timer2_.async_wait(asio::bind_executor(strand_,
-            std::bind(&printer::print, this, asio::placeholders::error, &timer2_)));
-    }
-    ~printer() {
-        Log::Info(std::format("Final count is {}", count_));
-    }
-
-    void print(const std::error_code& ec, asio::steady_timer* timer)
-    {
-        if (count_ < 5) {
-            Log::Info(std::format("print: [{}] {}", count_, ec.message()));
-            ++count_;
-
-            timer->expires_at(timer->expiry() + asio::chrono::seconds(1));
-            timer->async_wait(asio::bind_executor(strand_,
-                std::bind(&printer::print, this, asio::placeholders::error, timer)));
-        }
-    }
-
-private:
-  asio::strand<asio::io_context::executor_type> strand_;
-  asio::steady_timer timer1_;
-  asio::steady_timer timer2_;
-  int count_;
-};
-
+using asio::ip::tcp;
 
 int main(int argc, char** argv)
 {
     Boot::LogInfo(argc, argv);
 
-    asio::io_context io;
-    printer p(io);
-    std::thread t([&]{ io.run(); });
-    io.run();
-    t.join();
+    // try {
+        if (argc != 2) {
+            std::cerr << "Usage: client <host>" << std::endl;
+            std::cerr << "Service can be emulated: `while true; do echo -ne \"$(date -u)\\0\" | nc -l 13; echo accessed; done`" << std::endl;
+            return 1;
+        }
+        asio::io_context io_context;
+        tcp::resolver resolver(io_context);
+        tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
+        tcp::socket socket(io_context);
+        asio::connect(socket, endpoints);
+        for (;;) {
+            std::array<char, 128> buf;
+            std::error_code error;
 
+            size_t len = socket.read_some(asio::buffer(buf), error);
+            if (error == asio::error::eof) {
+                std::cout << "closed normally\n";
+                break; // Connection closed cleanly by peer.
+            } else if (error) {
+                //throw std::system_error(error); // Some other error.
+                std::cerr << "closed w/ error: " << error.message() << std::endl;
+                break;
+            }
+
+            std::cout.write(buf.data(), len);
+            std::cout << "\n";
+        }
+    // } catch(std::exception ex) {
+    //     std::cerr << "EXCEPTION: " << ex.what();
+    // }
     return 0;
 }
